bb3c5897d9d5c6b246b04ed2ccf88a699f97f8a1
Alexsey <83961167+AlexseySilva@users.noreply.github.com>
2022-08-30 14:48:47 -0300

    Duas soluções para bebida

--- bebida 01.py
+++ bebida 01.py
@@ -5,4 +5,11 @@ elif('bre' in cod_beb):
    print("bebida refrigerante")
 else:
    print("não cadastrada")
-
+#
+#
+#  Solução 2
+cod_beb = input("digite o codigo da bebida:")
+if('bac' in cod_beb):
+     print('bebida alcolica')
+else:
+    print('bebida refrigerante')

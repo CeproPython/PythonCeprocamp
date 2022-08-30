19a9341d0a3da248ddaada380ebdb3b8d1f4debf
Alexsey <83961167+AlexseySilva@users.noreply.github.com>
2022-08-30 14:42:52 -0300

    Colocando comentarios em tudo

--- while.py
+++ while.py
@@ -1,8 +1,8 @@
-s = 0 
-while True:
-    v = int(input("Digite um numero para somar ou 0 para sair :"))
-    if v == 0:
-        break
-    s = s +v 
-    print(s)
+s = 0 #<<< Variavel
+while True: #Enquanto verdade!
+    v = int(input("Digite um numero para somar ou 0 para sair :")) # V vira variavel
+    if v == 0: # Se V (==)igual a 0 pare
+        break #(Break)pare
+    s = s +v # S igual a S + V
+    print(s) #imprima o S
     
